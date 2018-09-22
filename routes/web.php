5ca2d7e82dbec6b9fa8426216a95758d87c8609c
idmc <id.morales10n@gmail.com>
2018-09-21 22:48:40 -0500

    utilizando nuevo middleware verified

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
 
 Auth::routes(['verify'=>true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
